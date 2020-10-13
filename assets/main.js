0fe260a55a95d34a5762c8789d95da0553f7c649
FrancescoDale <fd060862@gmail.com>
2020-10-13 14:35:42 +0200

    nuove variabili, calcolo del costo pieno del tragitto

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,14 @@
 var userDistance;
+var price;
+var fullPrice;
+
 var userAge;
 var userDiscount;
+
+price = 0.21;
+userDistance = prompt(' inserire la lunghezza del tragitto in km :');
+userAge = prompt(' inserire la votra età : ');
+
+fullPrice = userDistance * price;
+
+document.getElementById('fullprice').innerHTML = fullPrice;
