0a47d2ab1d3628916e3f5de78bb379f12d7e31fa
FrancescoDale <fd060862@gmail.com>
2020-10-13 14:44:11 +0200

    sconto minorenni

--- assets/main.js
+++ assets/main.js
@@ -12,3 +12,11 @@ userAge = prompt(' inserire la votra età : ');
 fullPrice = userDistance * price;
 
 document.getElementById('fullprice').innerHTML = fullPrice;
+
+// calcolo delle percentuali di sconto
+
+if (userAge<18) {
+    userDiscount = fullPrice * 0.80;
+    document.getElementById('discountedprice').innerHTML = userDiscount;
+
+}
